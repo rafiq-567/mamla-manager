db194a34b9b0bc665bf5c338bb240545b101977d
unknown <mdrafiqulakhter0@gmail.com>
2026-02-20 16:53:26 +0600

    small fix

--- app/api/clients/[id]/route.ts
+++ app/api/clients/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
-import Client from '@/lib/models/Client';
+import Client from '@/lib/models/Clients';
 import { getCurrentUser } from '@/lib/auth';
 
 // GET - Single client
